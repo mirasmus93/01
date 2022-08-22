881f5ff42ceb6221bdc5e06e262a69f046a32c06
mirasmus93 <111749407+mirasmus93@users.noreply.github.com>
2022-08-22 18:07:11 +0600

    Update gcd.go

--- gcd.go
+++ gcd.go
@@ -1,12 +1,14 @@
-package main
+package 01
 
-import (
-	"fmt"
-	"os"
-	"strconv"
-)
+import "fmt"
 
-func main() {
+func gcd(){
+	fmt.Println(" // import ( 
+		    "fmt"
+		    "os"
+		    "strconv"
+		   )
+	func main() {
 	args := os.Args[1:]
 	v1, _ := strconv.Atoi(args[0])
 	v2, _ := strconv.Atoi(args[1])
@@ -15,15 +17,15 @@ func main() {
 		fmt.Println()
 	} else {
 		gcd(v1, v2)
+		}
 	}
-}
-
-func gcd(num1 int, num2 int) {
+	
+	func gcd(num1 int, num2 int) {
 	var gcdnum int
 	for i := 1; i <= num1 && i <= num2; i++ {
 		if num1%i == 0 && num2%i == 0 {
 			gcdnum = i
+			}
 		}
-	}
 	fmt.Println(gcdnum)
-}
+	}")
